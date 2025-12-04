c60f44df5a20eaf5c53a431bb1b26e9303f048ae
JonasThowsen <jonas.aa.thowsen@gmail.com>
2025-12-04 12:12:25 +0100

    Second puzzle day one

--- 2025/1.exs
+++ 2025/1.exs
@@ -1,4 +1,4 @@
-defmodule DayOne do
+defmodule DayOnePart1 do
   @modulus 100
 
   def run() do
@@ -14,8 +14,9 @@ defmodule DayOne do
         {new_dial, new_count}
       end)
 
+    IO.puts("Part 1:")
     IO.puts("Final dial: #{final_dial}")
-    IO.puts("Times pointing at 0: #{times_at_zero}")
+    IO.puts("Times pointing at 0 after rotations: #{times_at_zero}")
   end
 
   defp apply_turn(dial, "L", clicks) do
@@ -27,4 +28,43 @@ defmodule DayOne do
   end
 end
 
-DayOne.run()
+DayOnePart1.run()
+
+defmodule DayOnePart2 do
+  @modulus 100
+
+  def run() do
+    {final_dial, total_hits} =
+      File.stream!("./day1input")
+      |> Enum.reduce({50, 0}, fn line, {dial, count} ->
+        line = String.trim(line)
+        {dir, clicks_str} = String.next_grapheme(line)
+        clicks = String.to_integer(clicks_str)
+
+        {new_dial, hits} = apply_turn(dial, dir, clicks)
+        {new_dial, count + hits}
+      end)
+
+    IO.puts("Part 2:")
+    IO.puts("Final dial: #{final_dial}")
+    IO.puts("Total hits on 0: #{total_hits}")
+  end
+
+  defp apply_turn(dial, _dir, 0), do: {dial, 0}
+
+  defp apply_turn(dial, "L", clicks) do
+    new_dial = Integer.mod(dial - 1, @modulus)
+    hits = if new_dial == 0, do: 1, else: 0
+    {final_dial, remaining_hits} = apply_turn(new_dial, "L", clicks - 1)
+    {final_dial, hits + remaining_hits}
+  end
+
+  defp apply_turn(dial, "R", clicks) do
+    new_dial = Integer.mod(dial + 1, @modulus)
+    hits = if new_dial == 0, do: 1, else: 0
+    {final_dial, remaining_hits} = apply_turn(new_dial, "R", clicks - 1)
+    {final_dial, hits + remaining_hits}
+  end
+end
+
+DayOnePart2.run()
